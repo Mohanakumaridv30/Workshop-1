deca25cf0c49c953fe01a7c80829f4939e679a0c
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-01-13 10:20:46 +0530

    Swapping two numbers with out third variable

--- src/com/bridgedlabz/workshop/SwapTwoNumbers.java
+++ src/com/bridgedlabz/workshop/SwapTwoNumbers.java
@@ -1,2 +1,18 @@
-package com.bridgedlabz.workshop;public class SwapTwoNumbers {
+package com.bridgedlabz.workshop;
+
+import java.util.Scanner;
+public class SwapTwoNumbers {
+    public static void main(String arg[])
+    {
+        System.out.println("Enter the value of x and y");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println("before swapping: "+x+ " " + y);
+        x = x + y;
+        y = x - y;
+       x = x - y;
+
+        System.out.println("after swapping: "+x+ " " + y);
+    }
 }
